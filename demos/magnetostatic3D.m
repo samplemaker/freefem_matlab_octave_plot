fa28e4bf0baa3e64343b6f7ae04cb1c9223f9222
samplemaker <chloros2@gmx.de>
2018-12-10 10:26:01 +0100

    format code

--- demos/magnetostatic3D.m
+++ demos/magnetostatic3D.m
@@ -50,7 +50,8 @@ view([-47,24]);
 camlight('headlight');
 
 figure;
-ffpdeplot3D(p,b,t,'FlowData',[Bx,By,Bz],'FGridParam3D',[15,15,15],'Boundary','on','BDLabels',PHYSTORUSSUFACE,'XYZStyle','monochrome','FMode3D','random');
+ffpdeplot3D(p,b,t,'FlowData',[Bx,By,Bz],'FGridParam3D',[15,15,15],'Boundary','on', ...
+            'BDLabels',PHYSTORUSSUFACE,'XYZStyle','monochrome','FMode3D','random');
 ylabel('y');
 xlabel('x');
 zlabel('z');
@@ -64,7 +65,8 @@ S1=[0.0 -1.3 0.0];
 S2=[0.0 -1.3 2.0];
 S3=[0.0 1.3 0.0];
 figure;
-ffpdeplot3D(p,b,t,'FlowData',[Bx,By,Bz],'Slice',S1,S2,S3,'BoundingBox','on','BDLabels',PHYSTORUSSUFACE,'XYZStyle','monochrome','FGridParam',[25,25]);
+ffpdeplot3D(p,b,t,'FlowData',[Bx,By,Bz],'Slice',S1,S2,S3,'BoundingBox','on', ...
+            'BDLabels',PHYSTORUSSUFACE,'XYZStyle','monochrome','FGridParam',[25,25]);
 ylabel('y');
 xlabel('x');
 zlabel('z');
@@ -77,10 +79,16 @@ S1=[0.0 -1.3 0.0];
 S2=[0.0 -1.3 2.0];
 S3=[0.0 1.3 0.0];
 figure;
-ffpdeplot3D(p,b,t,'FlowData',[Jx,Jy,Jz],'Slice',S1,S2,S3,'BoundingBox','on','Boundary','off','FGridParam',[50,50]);
+ffpdeplot3D(p,b,t,'FlowData',[Jx,Jy,Jz],'Slice',S1,S2,S3,'BoundingBox','on', ...
+            'Boundary','off','FGridParam',[50,50]);
 
 S1=[0.0 -1.3 0.0; -1.3 1.3 1];
 S2=[0.0 -1.3 2.0; -1.3 -1.3 1];
 S3=[0.0 1.3 0.0; 1.3 1.3 1];
 figure;
-ffpdeplot3D(p,b,t,'XYZData',u,'Slice',S1,S2,S3,'SGridParam',[50,50],'Boundary','off','ColorMap',jet(150),'Colorbar','on','BoundingBox','on');
+ffpdeplot3D(p,b,t,'XYZData',u,'Slice',S1,S2,S3,'SGridParam',[50,50], ...
+            'Boundary','off','ColorMap',jet(150),'ColorRange','centered', ...
+            'Colorbar','on','BoundingBox','on');
+ylabel('y');
+xlabel('x');
+zlabel('z');
