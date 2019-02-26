99b8189ef1229f70ea3c771374669829a59b3980
samplemaker <chloros2@gmx.de>
2019-02-26 22:39:12 +0100

    merge from 7607961d70adeb3f4f94fd234531df8b102a1856

--- release-v2.0/demos/ffmatlib/ffextractfespace.m
+++ release-v2.0/demos/ffmatlib/ffextractfespace.m
@@ -1,9 +1,9 @@
 %%  Extract from vectorized FE-Space
 %
 %  Author: Chloros2 <chloros2@gmx.de>
-%  Created: 2019-01-06
+%  Created: 2019-02-26
 %
-% [varargout] = ffextractfespace(elementNo,VhType,nt,Vh)
+% [varargout] = ffextractfespace(elementNo,vhStruct,nt,Vh)
 %
 %  This file is part of the ffmatlib which is hosted at
 %  https://github.com/samplemaker/freefem_matlab_octave_plot
@@ -11,19 +11,23 @@
 
 %% Description
 %
-%  Gets individual FE-Space from a vectorized FE-Space
+%  Gets component data and component FE-Space from a vectorized FE-Space
 %
 
 %% Input Parameters
 %
-%  VhType:    Structure of the FE-Space. For "fespace Vh(Th, [P2,P1])" enter {'P2','P1'}
-%  nt:        Number of Meshtriangles
-%  vh:        Input FE-Space
+%  elementNo: Index of component to be extracted
+%  vhStruct:  Structure of the vectorized FE-Space.
+%             I.e. for "fespace Vh(Th, [P2,P1])" enter {'P2','P1'}
+%  nt:        Number of triangles
+%  vh:        Vector valued input FE-Space
+%  u:         Vector valued PDE solution
 %
 
 %% Output Parameters
 %
-%  varargout: The individual FE-Subspaces
+%  vhout: The component of the subspace/suborder
+%  uout:  The component of the PDE solution
 %
 
 %% Licence
@@ -47,42 +51,41 @@
 
 %% Code
 
-function [varargout] = ffextractfespace(VhType,nt,vh)
-
-    for i=1:numel(VhType)
-         %elementType = VhType{elementNo(i)};
-         varargout{i} = extract_fespace(i,VhType,nt,vh);
-    end
-
-end
-
 %Extract the FE-Space for a particular element from a vectorized FE-Space
-function [vhout] = extract_fespace(elementNo,VhType,nt,vhin)
+function [vhout,uout] = ffextractfespace(elementNo,vhStruct,nt,vhin,u)
 
+    if (elementNo > numel(vhStruct)) || (elementNo <= 0)
+        error('Invalid element Index');
+    end
+ 
     %DoFs of the implemented vector spaces
-    EnDoFs = {{'P1','P2'},{3,6}};
+    EnDoFs = {{'P1','P1b','P2'},{3,4,6}};
     pattern = [];
-    for i=1:numel(VhType)
-         idx = strcmpi(VhType{i},EnDoFs{1});
+    nDoF = [];
+    for i=1:numel(vhStruct)
+         idx = strcmpi(vhStruct{i},EnDoFs{1});
          if any(idx)
              j = find(idx,1);
              %n(i) = EnDoFs{2}{j};
              if i == elementNo
+                 nDoF = EnDoFs{2}{j};
                  pattern = [pattern, true(1,EnDoFs{2}{j})];
              else
                  pattern = [pattern, false(1,EnDoFs{2}{j})];
              end
          else
-             fprintf('Element %s is not implemented!\n',VhType{i});
+             fprintf('Element %s is not implemented!\n',vhStruct{i});
              error('Unknown Element');
          end
     end
     mask = repmat(logical(pattern)',nt,1);
-    vhout = vhin(mask);
+    vhMask = vhin(mask);
+    uout = u(vhMask+1);
+    vhout = linspace(0,nDoF*nt-1,nDoF*nt);
 
 end
 
 function printhelp()
-    fprintf('%s\n\n','Invalid call to ffreadfespace. Correct usage is:');
-    fprintf('%s\n',' -- [varargout] = ffreadfespace ()');
+    fprintf('%s\n\n','Invalid call to ffreadfespace(). Correct usage is:');
+    fprintf('%s\n',' -- [vhout,uout] = ffextractfespace(elementNo,vhStruct,nt,vhin,u)');
 end
---
+++ release-v2.0/demos/vectored_fespace.m
@@ -0,0 +1,53 @@
+% Author: Chloros2 <chloros2@gmx.de>
+% Created: 2019-02-26
+%
+% Copyright (C) 2018 Chloros2 <chloros2@gmx.de>
+%
+% This program is free software: you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see
+% <https://www.gnu.org/licenses/>.
+%
+
+clear all;
+
+addpath('ffmatlib');
+
+[p,b,t,nv,nbe,nt] = ffreadmesh('vectored_mesh.msh');
+%Vector valued space {'P2','P2','P1'}
+[vhVEC] = ffreaddata('vectored_vh.txt');
+%FreeFem++ feature: solutions u,v,w - they are all the same. Need only one component.
+[uVEC] = ffreaddata('vectored_data.txt');
+
+%Extract first component (PDE solution u and subspace/suborder for P2)
+[vhP2,u] = ffextractfespace(1, {'P2','P2','P1'}, nt, vhVEC, uVEC);
+%Extract second component - same suborder as for component #1
+[~,v] = ffextractfespace(2, {'P2','P2','P1'}, nt, vhVEC, uVEC);
+%Extract third component
+[vhP1,w] = ffextractfespace(3, {'P2','P2','P1'}, nt, vhVEC, uVEC);
+
+ffpdeplot(p,b,t,'VhSeq',vhP2,'XYData',u,'Mesh','on','MColor','b','Boundary','on','ColorMap',jet);
+ylabel('y');
+xlabel('x');
+figure;
+ffpdeplot(p,b,t,'VhSeq',vhP2,'XYData',v,'Mesh','on','MColor','b','Boundary','on','ColorMap',jet);
+ylabel('y');
+xlabel('x');
+figure;
+ffpdeplot(p,b,t,'VhSeq',vhP2,'FlowData',[u,v],'FGridParam',[24, 24],'Mesh','off','Boundary','on','ColorBar','off');
+ylabel('y');
+xlabel('x');
+axis tight;
+figure;
+ffpdeplot(p,b,t,'VhSeq',vhP1,'XYData',w,'Mesh','on','MColor','b','Boundary','on','ColorMap',jet);
+ylabel('y');
+xlabel('x');
